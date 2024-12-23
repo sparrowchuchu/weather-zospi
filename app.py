2d5d1f5d7e41c3d76a994745eb6ecba343a94a50
Sharon <102872908+sparrowchuchu@users.noreply.github.com>
2024-12-24 02:01:45 +0800

    FastAPI uvicorn app:app --reload

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+
+app = FastAPI(debug=True)
+
+# 設置靜態文件目錄
+app.mount("/static", StaticFiles(directory="static"), name="static")
+    
+# 設置模板目錄
+templates = Jinja2Templates(directory="templates")
+
+# Mount the static files directory to serve CSS, JS, and images
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    
